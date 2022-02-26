bfc64b8d41031623868f99409edb36e893944246
akatsuke <8422126+akatsukee@user.noreply.gitee.com>
2022-02-26 19:44:55 +0800

    first commit

--- go.mod
+++ go.mod
@@ -1,1 +1,9 @@
-module "quik"
+module quik
+
+go 1.16
+
+require (
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	go.uber.org/zap v1.21.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package mian
 
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func init() {
+
+}
+
 func main() {
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
 
 }
